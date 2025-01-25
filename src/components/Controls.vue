2e8a249d217aa2adf11cd1dacd61779d73fe40ea
Javier Rojas <jardlml1209@gmail.com>
2025-01-25 14:47:50 -0300

    refactor Controls component to support axis-based layout; update App.vue for dynamic control rendering

--- src/components/Controls.vue
+++ src/components/Controls.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="flex flex-col mt-5">
+  <div v-if="isAxisX" class="flex flex-row mt-2 md:mt-5 justify-center md:hidden">
+    <button @click="handleKeydown({ key: 'ArrowLeft' })">Left</button>
+    <button @click="handleKeydown({ key: 'ArrowUp' })">Rotate</button>
+    <button @click="handleKeydown({ key: 'ArrowRight' })">Right</button>
+  </div>
+  <div v-if="!isAxisX" class="flex flex-col mt-5 md:hidden">
     <button @click="drop">Drop</button>
     <button @click="hardDrop">Hard drop</button>
   </div>
@@ -10,6 +15,9 @@ import { mapActions } from "vuex";
 
 export default {
   name: "Controls",
+  props: {
+    isAxisX: Boolean,
+  },
   methods: {
     ...mapActions("game",["hardDrop", "handleKeydown"]),
     drop() {
@@ -22,6 +30,6 @@ export default {
 
 <style scoped>
 button {
-  @apply bg-gray-800 text-white p-2 m-2 rounded-md;
+  @apply bg-gray-800 text-white p-2 m-2 rounded-md flex-1;
 }
 </style>
