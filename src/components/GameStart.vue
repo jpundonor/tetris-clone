5fb6fea85dd65ae2027c05a04671ac0eee6f8fc9
Javier Rojas <jardlml1209@gmail.com>
2025-02-05 03:22:09 -0300

    feat: enhance game controls and add HoldPiece functionality

--- src/components/GameStart.vue
+++ src/components/GameStart.vue
@@ -1,33 +1,61 @@
 <template>
   <div v-if="!isStarted" class="game-start">
     <h1>TetrisClone</h1>
-    <p class="start-button" @click="startGame">Start Game</p>
+    <div class="buttons-container">
+      <button class="start-button" @click="startGame">Start Game</button>
+      <button class="control-button" @click="showModalControls">Controls</button>
+    </div>
+    <div v-show="showControls">
+      <UiControls />
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapState } from "vuex";
+import UiControls from "./UiControls.vue";
 
 export default {
   name: "GameStart",
+  components: {
+    UiControls,
+  },
+  data() {
+    return {
+      showControls: false,
+    };
+  },
   computed: {
     ...mapState("game", ["isStarted"]),
   },
   methods: {
     ...mapActions("game", ["startGame"]),
+    showModalControls() {
+      this.showControls = !this.showControls;
+    },
   },
 };
 </script>
 
 <style scoped>
 h1 {
-  @apply text-4xl font-bold mb-10 text-black;
+  @apply text-4xl font-bold mb-5 text-black;
 }
-.game-start{
+button{
+  @apply font-semibold px-4 py-2 rounded transition-all duration-500;
+}
+.game-start {
   @apply text-center fixed top-0 left-0 w-full h-full bg-slate-600 flex flex-col justify-center items-center z-10;
 }
+.buttons-container {
+  @apply flex flex-col gap-5;
+}
 .start-button {
-  @apply text-white bg-gray-900 font-semibold px-4 py-2 rounded cursor-pointer transition-all duration-500;
+  @apply text-white bg-gray-900;
   @apply hover:bg-gray-200 hover:text-gray-900;
+}
+.control-button {
+  @apply text-gray-900 bg-transparent border;
+  @apply hover:bg-gray-700 hover:text-white;
 }
 </style>
